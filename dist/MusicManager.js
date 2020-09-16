6bf581439472b7cbfd8af00cb0140355350243b4
Amir-Benyamini <amir.benyamini.ppc@gmail.com>
2020-09-16 18:56:09 +0300

    like/dislike updates and more

--- dist/MusicManager.js
+++ dist/MusicManager.js
@@ -1,75 +1,113 @@
 const countries = ["algeria", "afganistan", "Bahamans"]
-const dummySongs =
-	[{
-		name: "Billie Jean",
-		playCount: 1223242432,
-		bpm: 0,
-		spotifyId: "0dQSOy6CUlC4cS9AjfC904",
-		lastFmLink: "https://www.last.fm/music/Michael+Jackson/_/Billie+Jean",
-		youtubeId:"Zi_XLOBDo_Y",
-		album: {
-			name: "Thriller",
-			releaseDate:"30-11-1982",
-			artist:{
-				name:"Michael Jackson",
-				country:"United States",
-				wikipediaSummary:"https://en.wikipedia.org/wiki/Michael_Jackson"
-			}
-		}
-	},
-	{
-		name: "Billie Jean",
-		playCount: 1223242432,
-		bpm: 0,
-		spotifyId: "0dQSOy6CUlC4cS9AjfC904",
-		lastFmLink: "https://www.last.fm/music/Michael+Jackson/_/Billie+Jean",
-		youtubeId:"Zi_XLOBDo_Y",
-		album: {
-			name: "Thriller",
-			releaseDate:"30-11-1982",
-			artist:{
-				name:"Michael Jackson",
-				country:"United States",
-				wikipediaSummary:"https://en.wikipedia.org/wiki/Michael_Jackson"
-			}
-		}
-	},
-	{
-		name: "Billie Jean",
-		playCount: 1223242432,
-		bpm: 0,
-		spotifyId: "0dQSOy6CUlC4cS9AjfC904",
-		lastFmLink: "https://www.last.fm/music/Michael+Jackson/_/Billie+Jean",
-		youtubeId:"Zi_XLOBDo_Y",
-		album: {
-			name: "Thriller",
-			releaseDate:"30-11-1982",
-			artist:{
-				name:"Michael Jackson",
-				country:"United States",
-				wikipediaSummary:"https://en.wikipedia.org/wiki/Michael_Jackson"
-			}
-		}
-	},
-	{
-		name: "Billie Jean",
-		playCount: 1223242432,
-		bpm: 0,
-		spotifyId: "0dQSOy6CUlC4cS9AjfC904",
-		lastFmLink: "https://www.last.fm/music/Michael+Jackson/_/Billie+Jean",
-		youtubeId:"Zi_XLOBDo_Y",
-		album: {
-			name: "Thriller",
-			releaseDate:"30-11-1982",
-			artist:{
-				name:"Michael Jackson",
-				country:"United States",
-				wikipediaSummary:"https://en.wikipedia.org/wiki/Michael_Jackson"
-			}
-		}
-	}
+const getDummySongs = function() {
+	return [
+    {
+        "_id": "5f62253d56c2e5a5fd6bf5cb",
+        "name": "Az Man Begurezed",
+        "playCount": 3556,
+        "bpm": 86.862,
+        "spotifyId": "1km5bnYhzfFK0qrs5hLRYa",
+        "lastFmLink": "https://www.last.fm/music/Ahmad+Zahir/_/Az+Man+Begurezed",
+        "youtubeId": null,
+        "album": {
+            "_id": "5f62253d56c2e5a5fd6bf5ca",
+            "name": "The Kite Runner",
+            "releaseDate": "2007-01-01T00:00:00.000Z",
+            "artist": {
+                "_id": "5f61c520f5acfef50dc24256",
+                "name": "Ahmad Zahir",
+                "country": "5f61c520f5acfef50dc24255",
+                "wikipediaSummary": "https://en.wikipedia.org/wiki/Ahmad_Zahir",
+                "__v": 0
+            }
+        }
+    },
+    {
+        "_id": "5f62253d56c2e5a5fd6bf5cd",
+        "name": "Jhaptal/dadra",
+        "playCount": 1416,
+        "bpm": 106.468,
+        "spotifyId": "1Z4NxUHOoeVV8xwv20JBBy",
+        "lastFmLink": "https://www.last.fm/music/Aziz+Herawi/_/Jhaptal%2Fdadra",
+        "youtubeId": "GVSH6gsj-KM",
+        "album": {
+            "_id": "5f62253d56c2e5a5fd6bf5cc",
+            "name": "Master Of Afghani Lutes",
+            "releaseDate": "1992-01-01T00:00:00.000Z",
+            "artist": {
+                "_id": "5f61c520f5acfef50dc2425a",
+                "name": "Aziz Herawi",
+                "country": "5f61c520f5acfef50dc24255",
+                "wikipediaSummary": "https://en.wikipedia.org/wiki/Aziz_Herawi",
+                "__v": 0
+            }
+        }
+    },
+    {
+        "_id": "5f62253d56c2e5a5fd6bf5cf",
+        "name": "Keliwali in the melodic mode of kastori",
+        "playCount": 756,
+        "bpm": 100.48,
+        "spotifyId": "09SMlD15piqWpsFju84FoO",
+        "lastFmLink": "https://www.last.fm/music/Ustad+Mohammad+Omar/_/Keliwali+in+the+melodic+mode+of+kastori",
+        "youtubeId": null,
+        "album": {
+            "_id": "5f62253d56c2e5a5fd6bf5ce",
+            "name": "Ustad Mohammad Omar: Virtuoso from Afghanistan",
+            "releaseDate": "2002-04-23T00:00:00.000Z",
+            "artist": {
+                "_id": "5f61c520f5acfef50dc2425c",
+                "name": "Ustad Mohammad Omar",
+                "country": "5f61c520f5acfef50dc24255",
+                "wikipediaSummary": "https://en.wikipedia.org/wiki/Mohammad_Omar_(musician)",
+                "__v": 0
+            }
+        }
+    },
+    {
+        "_id": "5f62253d56c2e5a5fd6bf5d1",
+        "name": "Khursheed-i-Mann - Live",
+        "playCount": 88,
+        "bpm": 114.677,
+        "spotifyId": "64Wdec5ZqrLtPxoZ1dMW8X",
+        "lastFmLink": "https://www.last.fm/music/Sarban/_/Khursheed-i-Mann+-+Live",
+        "youtubeId": null,
+        "album": {
+            "_id": "5f62253d56c2e5a5fd6bf5d0",
+            "name": "Live in Concert",
+            "releaseDate": "2014-09-29T00:00:00.000Z",
+            "artist": {
+                "_id": "5f61c520f5acfef50dc2425e",
+                "name": "Sarban",
+                "country": "5f61c520f5acfef50dc24255",
+                "wikipediaSummary": "https://en.wikipedia.org/wiki/Sarban",
+                "__v": 0
+            }
+        }
+    },
+    {
+        "_id": "5f62253d56c2e5a5fd6bf5d3",
+        "name": "Sheikh Ahmad-e Jâm",
+        "playCount": 487,
+        "bpm": 112.467,
+        "spotifyId": "6GL9Lj6F1pN69nWHlWCvCz",
+        "lastFmLink": "https://www.last.fm/music/Mohammad+Rahim+Khushnawaz/_/Sheikh+Ahmad-e+J%C3%A2m",
+        "youtubeId": null,
+        "album": {
+            "_id": "5f62253d56c2e5a5fd6bf5d2",
+            "name": "Afghanistan: Le rubâb de Hérat – Afghanistan: The Rubâb of Herat",
+            "releaseDate": "1993-01-01T00:00:00.000Z",
+            "artist": {
+                "_id": "5f61c520f5acfef50dc24260",
+                "name": "Mohammad Rahim Khushnawaz",
+                "country": "5f61c520f5acfef50dc24255",
+                "wikipediaSummary": "https://www.last.fm/music/Mohammad+Rahim+Khushnawaz",
+                "__v": 0
+            }
+        }
+    }
 ]
-
+}
 
 	
 class MusicManager {
@@ -91,18 +129,31 @@ class MusicManager {
 		restOfList.forEach(song => this.songs.push(song))
 	}
 
-	getSongByFilters = function (country, popular) {
+	getSongByFilters(dummy, bpm) {
 		// let songsList = await $.get(`/songs/${country}/?popular=${popular}`)
-		this.playSongs(country)
+		// let songsList = await $.get(`/songs/${country}`)
 
+		if (bpm === "fast"){
+			this.songs = []
+			const fastSongs = dummy.filter(song => song.bpm >= 100)
+			this.playSongs(fastSongs)
+		} else if(bpm === "slow"){
+			this.songs = []
+			const slowSongs =	dummy.filter(song => song.bpm <= 100)
+			this.playSongs(slowSongs)
+		} else{
+			this.songs = []
+			this.playSongs(dummy)
+		}
+		
 		return this.songs
 
 	}
 
 	getSongsFromDB = function (dummy) {
 		// let myPlaylist = await $.get(`/songs/myPlaylist`)
+		this.songs = [] 
 		this.playSongs(dummy)
-
 		return this.songs
 	}
 
--- dist/main.js
+++ dist/main.js
@@ -4,17 +4,16 @@ const renderer = new Renderer
 $('#countryInpBtn').on('click', function () {
 	const countryInput = $('#countryInp').val()
 	const PopularInput = $('#popularInp').is(':checked')
-	const radioValue = $("input[name='bpm']:checked").val();
-	
-	// const songsList = manager.getSongByFilters(countryInput, PopularInput, radioValue)
-	const songsList = manager.getSongByFilters(dummySongs)
+	const bpm = $("input[name='bpm']:checked").val();
+	// const songsList = manager.getSongByFilters(countryInput, PopularInput, bpm)
+	const songsList = manager.getSongByFilters(getDummySongs(), bpm)
 	renderer.renderSongs(songsList)
 
 })
 
-$('#playlist').on('click', function(){
-  const songsList = manager.getSongsFromDB(dummySongs)
-  renderer.renderMyPlaylist(songsList)
+$('#playlist').on('click', function () {
+	const songsList = manager.getSongsFromDB(getDummySongs())
+	renderer.renderMyPlaylist(songsList)
 })
 
 // on page load add countries to select dropdown
@@ -24,13 +23,33 @@ $(document).ready(function () {
 })
 
 //find song name and artist, then find the song object and save it to DB
-$('songs').on('click', '#like', function(){
-	const songTitle = $(this).closest('.now-playing').find('.title')
-	const songArtist = $(this).closest('.now-playing').find('.artist')
-	
-	manager.this.songs.forEach(song => {
-		if(songTitle === song.song && songArtist === song.artist){
+$('.songs').on('click', '#like', function () {
+	const songId = $(this).closest('.song').data().id
+
+	manager.songs.forEach(song => {
+		if (songId === song._id) {
 			manager.saveSongToDB(song)
+			alert(`${song.name} was added to your playlist!`)
+		} else {
+			console.log('error')
 		}
-	});
+	})
 })
+
+$('.songs').on('click', '#dislike', function () {
+
+	const songId = $(this).closest('.song').data().id
+	manager.songs.forEach(song => {
+		if (songId === song._id) {
+			manager.deleteSongToDB(song._id)
+
+			const songsList = manager.getSongsFromDB(getDummySongs())
+			renderer.renderMyPlaylist(songsList)
+			alert(`${song.name} was deleted from your playlist!`)
+		} else {
+			console.log('error')
+		}
+	})
+
+})
+
